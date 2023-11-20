c4591b2c9b5e08c6bc2b558b98bb2ba6f48e8352
Rafael Reis Franco <rafaelreisfranco15@gmail.com>
2023-11-20 19:11:28 -0300

    :trophy: Optimizations

--- src/app/login/components/ModalLostPassword/ModalLostPassword.style.js
+++ src/app/login/components/ModalLostPassword/ModalLostPassword.style.js
@@ -23,7 +23,7 @@ export const OverlayModal = styled.div`
     align-items: center;
     justify-content: center;
     z-index: 2023;
-    position: absolute;
+    position: fixed;
     top: 50%;
     left: 50%;
     translate: -50% -50%;
